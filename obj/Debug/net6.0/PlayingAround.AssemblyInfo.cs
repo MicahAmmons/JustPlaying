418e658c9c3a19d4d7d89e18a6ecfe5b7ed4ce12
Micah Ammons <micahammons@gmail.com>
2025-05-29 12:16:07 -0400

    Ye

--- obj/Debug/net6.0/PlayingAround.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlayingAround.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayingAround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a121545c1c059dbc65cfbdd090cbfdb9330d72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c792668e8282680684514a672877a5fcbfc8a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayingAround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayingAround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
